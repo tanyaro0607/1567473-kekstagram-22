aa8161e6c7be986919839f81bbe5ab490f5decae
Tanya <ta.rohlova@yandex.ru>
2021-02-12 20:08:28 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const getPossibleRowSize = (rowCheck, maxRowSize) => {
 alert(getPossibleRowSize('2',140));
 
 //HW
-/* 
+/*
 const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -29,6 +29,22 @@ const getRandomArray = () => {
 }
 */
 
+//получение массива элементов
+/*const array = [],
+  min = 1,
+  max = 25;
+
+for(let i = 1; i <= max; i++) array.push(i);
+array.splice(arr.indexOf(Math.floor(Math.random() * (max + 1 - min) + min)),1);
+array.sort(function() { return Math.random() - 0.5});
+//console.log(arr);
+
+//перебор по одному
+for (let i = 0; i <= array.length - 1; i++) {
+  array[i];
+}*/
+
+//исходные данные
 const NAMES = [
   'Иван',
   'Алексей',
@@ -65,53 +81,47 @@ const DESCRIPTION = [
 const POST_COUNT = 25; // всего элементов
 
 // Получение случайного числа в диапозоне(включительно)
-// Источник https://developer.mozilla.org/
 const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//id - сделать массив, чтоб числа были уникальны и брать число из него
-
-// url - адрес photos/{{i}}.jpg, i - число от 1 до 25 без повтора - создать массив
-
-//Avatar - сделать массив
+//id, url, Avatar- сделать массив, чтоб числа были уникальны и брать число из него
 
 //из лекции - получить рандомный элемент из массива - ошибка lodash при проверке
 /*const getRandomArrayElement = (elements) => {
   return elements[_.random(0, elements.length - 1)];
 }; */
-//это одно и тоже, что выше?
+
+//получить рандомный элемент из массива
 const getRandomArrayElement = (elements) =>{
   return elements[getRandomInteger(0, elements.length - 1)];
 };
 
-//получение массива элементов
-const arr = [],
-  min = 1,
-  max = 25;
-
-for(let i = 1; i <= max; i++) arr.push(i);
-arr.splice(arr.indexOf(Math.floor(Math.random() * (max + 1 - min) + min)),1);
-arr.sort(function() { return Math.random() - 0.5});
-//console.log(arr);
-
-//перебор по одному
-for (let i = 0; i <= arr.length - 1; i++) {
-  arr[i];
+// Функция генерируящая id фотографии, номер фото url, avatar
+let getRandonValue = (minValue, maxValue, length) => {
+  let randomValue = [];
+  while(randomValue.length < length) {
+    const random = getRandomInteger(minValue, maxValue);
+    if (randomValue.indexOf(random) === -1) {
+      randomValue.push(random);
+    }
+  }
+  return randomValue;
 }
+getRandonValue();
 
 //функция + описание
 const createPost = () => {
   return {
-    id: getRandomInteger(1,25), //исправить на уникальный
-    url: 'photos/'+getRandomInteger(1,25)+'.jpg', //исправить на уникальный
+    id: getRandonValue(1,25), // уникальный?
+    url: 'photos/'+getRandonValue(1,25)+'.jpg', //уникальный?
     description: getRandomArrayElement(DESCRIPTION),
     likes: getRandomInteger(15,200), //ок
     comments: [{
-      id: getRandomInteger(100,999), //исправить на уникальный
-      avatar: 'img/avatar-'+getRandomInteger(1,6)+'.svg', //исправить на уникальный
+      id: getRandonValue(100,999), //уникальный?
+      avatar: 'img/avatar-'+getRandonValue(1,6)+'.svg', //исправить на уникальный
       message: getRandomArrayElement(COMMENTS), //OK
       name: getRandomArrayElement(NAMES), //OK
     }],
