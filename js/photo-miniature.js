7b21e1b433f54ae7153107fc8b86f946abce3803
Tanya <ta.rohlova@yandex.ru>
2021-03-13 20:54:57 +0300

    Отрисуй меня полностью (часть 2)

--- js/photo-miniature.js
+++ js/photo-miniature.js
@@ -7,19 +7,18 @@ const containerForPhotos = document.querySelector('.pictures'); //Контейн
 const similarListFragment = document.createDocumentFragment(); //cписок, в который мы будем вставлять фото пользователей
 const similarPhotoTemplate = document.querySelector('#picture').content; // шаблон, который мы будем копировать
 
-// для проверки копирования
-//const photoElement = similarPhotoTemplate.cloneNode(true); //копируем с содержимым
-//similarListElement.appendChild(photoElement); // вставляем в конец списка
-
 const similarPhotos = createPosts();
 
-similarPhotos.forEach(({url, likes, comments}) => {
+similarPhotos.forEach(({url, likes, comments, id}) => {
   const photoElement = similarPhotoTemplate.cloneNode(true);  //копируем с содержимым
   photoElement.querySelector('.picture__img').src = url; //Адрес изображения url подставьте как src изображения
   photoElement.querySelector('.picture__likes').textContent = likes; //Количество лайков likes подставьте как текстовое содержание элемента .picture__likes
   photoElement.querySelector('.picture__comments').textContent = comments.length; //Количество комментариев comments подставьте как текстовое содержание элемента .picture__comments.
   similarListFragment.appendChild(photoElement); // фото вставляем в конец списка
+  let pictureNode = similarListFragment.lastElementChild;//находит элемент
+  pictureNode.dataset.id = id; // доб атрибут
 });
 
 containerForPhotos.appendChild(similarListFragment);
 
+export {similarPhotos};
